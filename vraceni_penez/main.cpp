c4e6a0307cd825ee4b61636a55c4ef984544b397
Lukas_Sandtner <sandtner05@gmail.com>
2024-10-26 19:25:43 +0200

    Update main.cpp

--- vraceni_penez/main.cpp
+++ vraceni_penez/main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
@@ -13,14 +5,15 @@ using namespace std;
 int main()
 {
     int penize;
-    cout << "Zadej penize: ";
+    cout << "Zadej penize (0 pro konec): ";
     cin >> penize;
     
-    for (int platidlo: {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}) {
-        int pocet = penize / platidlo;
-        penize = penize % platidlo;
-        cout << platidlo << ": " << pocet << endl;
-    }
-    
-    
+    do{
+        for (int platidlo: {5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1}) {
+            int pocet = penize / platidlo;
+            penize = penize % platidlo;
+            cout << platidlo << ": " << pocet << endl;
+        }
+    } while(penize != 0);
+    cout << "KONEC";
 }
