80bbcbdae11c3f8c1fa158af69e6ae3348a22dab
Lukas_Sandtner <sandtner05@gmail.com>
2024-11-01 20:53:46 +0100

    Update main.cpp

--- c++/trojuhelnik_2_0/main.cpp
+++ c++/trojuhelnik_2_0/main.cpp
@@ -6,10 +6,8 @@ Welcome to GDB Online.
   Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
-#include <stdio.h>
 #include <cmath>
 #include <iostream>
-#include <conio.h>
 
 using namespace std;
 
@@ -17,9 +15,12 @@ int main()
 {
     int volba;
     double a, b, c;
-
-    cout << "Vyberte moznost 1 (Neznate preponu) nebo moznost 2 (neznate odvesnu): ";
+while(true){
+    cout << "Vyberte možnost 1 (Neznáte přeponu), možnost 2 (neznáte odvěsnu) nebo 0 pro konec: ";
     cin >> volba;
+    if(volba == 0){
+        cout << "KONEC";
+    }    
     
     if(volba == 1) {
         cout << "Strana a: ";
@@ -28,27 +29,26 @@ int main()
         cin >> b;
         if(a != 0 && b != 0) {
             c = sqrt(a * a + b * b);
-            cout << "Prepona je: " << c;
+            cout << "Přepona je: " << c << endl;
         } else {
-            printf("Strana nesmi byt nulova!");
+            printf("Strana nesmí být nulová!\n");
         }
     } else if(volba == 2) {
         //Názvy proměnných nemusí odpovídat reálným stranám projúhelníku (a != b)
-        cout << "Odvesna: ";
+        cout << "Odvěsna: ";
         cin >> a;
-        cout << "Prepona: ";
+        cout << "Přepona: ";
         cin >>  c;
         
         if(a < c && a != 0 && c != 0) {
             b = sqrt(c * c - a * a);
-            cout << "Odvesna je: " << b;
+            cout << "Odvěsna je: " << b << endl;
         } else {
-            printf("Odvesna nesmi byt vetsi nez prepona nebo nesmi byt hodnota nulova!");    
+            printf("Odvěsna nesmí být větší než přepona nebo nesmí být hodnota nulová\n!");    
         }
         
-    } else if(volba != 1 || volba != 2) {
-        printf("Vyberte volbu 1 nebo 2!");
+    } else if(volba != 1 || volba != 2 || volba != 0) {
+        printf("Vyberte volbu 0, 1 nebo 2!\n");
     }
-    cout << endl << "Stisknete klavesu pro konec" << endl;
-    getch();
+}
 }
