9f426ee57bd4db6a54f753838759594d356c4526
Lukas_Sandtner <sandtner05@gmail.com>
2024-11-03 15:50:34 +0100

    Update main.cpp

--- testik/main.cpp
+++ testik/main.cpp
@@ -1,20 +1,11 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
-#include <iomanip>
 
 using namespace std;
 int main()
 {
 	int x[] = {5, 4, 8, 45, 20, 8};
 	int velikostPole = sizeof(x)/sizeof(x[0]);
-	for(int o = 0; o < velikostPole - 2; o++) {
+	for(int o = 0; o < velikostPole - 1; o++) {
 		if(x[o]%2 == 0) {
 			x[o] += 1;
 		} else {
@@ -29,15 +20,20 @@ int main()
 		cout << zmena << " ";
 	}
 	int pole1[] = {5,4,8,45,65,8};
+	int length = sizeof(x)/sizeof(pole1[0]);
+	int min = pole1[0];
+	for (int i = 0; i < length; i++) {
+	    if(pole1[i] < min){
+	        min = pole1[i];
+	    }
+	}
+	
 	int y = 0;
 	while(true) {
-		if(y == 6) {
-			y = 0;
-		}
 
-		pole1[y] = pole1[y] + 1;
+		min = min + 1;
 
-		if(pole1[y] == 100) {
+		if(min == 100) {
 			break;
 		}
 		y++;
@@ -46,14 +42,26 @@ int main()
 	for(int zmena1 : pole1) {
 		cout << zmena1 << " ";
 	}
-    
-    cout << endl <<"zadej faktorial: ";
-    int f;
-    cin >> f;
-    
-    for (int i = f-1; i >= 2; i--) {
-        f = f * i;
-    }
-    cout << f;
+
+	cout << endl <<"zadej faktorial: ";
+	int f;
+	cin >> f;
+
+	for (int i = f-1; i >= 2; i--) {
+		f = f * i;
+	}
+	cout << f;
+
+	int A; // deklarace promennych (celociselny typ)
+	cin >> A; // cteni cisl ze vstupu
+	while (A<10) { // podmC-nka ukonceni cyklu
+		A = A + 1;
+		if (A == 8) { // pri dosazeni A==8
+			break; // while konci v tomto miste! (cout << A se uz neprovede!!)
+		}
+		cout << A << " ";
+	}
+	cout << A;
 }
+
 
