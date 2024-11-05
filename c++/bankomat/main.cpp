fbbe0061146b2ef586fded46a4d840e06400a0bc
Lukas_Sandtner <sandtner05@gmail.com>
2024-11-05 20:33:35 +0100

    Update main.cpp

--- c++/bankomat/main.cpp
+++ c++/bankomat/main.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <iostream>
 
 using namespace std;
@@ -13,23 +5,28 @@ using namespace std;
 int main()
 {
     int senzor, pin, castka;
+    while(true){
+        cout << "Vložte kartu" << endl << "************" << endl << "Vložena? ANO-1 || NE-0: ";
+        cin >> senzor;
     
-    cout << "Vložte kartu" << endl << "************" << endl << "Vložena? ANO-1 || NE-0: ";
-    cin >> senzor;
-    if(senzor == 1) {
-        cout << "Zadejte PIN: ";
-        cin >> pin;
-        if(pin == 1234) {
-            cout << "Zadejte částku: ";
-            cin >> castka;
-            printf("Částka %i vyplacena", castka);
-            cout << "\nKonec";
-        }else {
-            cout << "Chybný PIN!";
-        } 
-    }else {
-        cout << "Konec";
+        if(senzor == 1) {
+            while(true){
+            cout << "Zadejte PIN: ";
+            cin >> pin;
+            if(pin == 1234){
+                cout << "Zadejte částku: ";
+                cin >> castka;
+                printf("Částka %i vyplacena", castka);
+                cout << "\nKonec\n";
+                break;
+            }else{
+                cout << "Chybný PIN!\n";
+                continue;
+            }
+            }
+        } else {
+            cout << "Konec";
+            break;
+        }
     }
-
-    return 0;
 }
